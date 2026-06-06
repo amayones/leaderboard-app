29d8593b2ed33458414ed4296d9c903aace32afa
amayones <mayonedrive1@gmail.com>
2026-06-06 12:30:51 +0700

    bucket fix

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,7 +1,7 @@
 import redis
 import os
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+REDIS_URL = os.getenv("REDIS_URL")
 redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
 
 def get_redis():
